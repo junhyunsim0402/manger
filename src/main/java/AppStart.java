bf3668b4e8d686af3ebc703f6fbe7b6a32c37bef
junhyunsim0402 <simjjang11@gmail.com>
2026-02-27 15:07:52 +0900

    중간 푸쉬

--- src/main/java/AppStart.java
+++ src/main/java/AppStart.java
@@ -1,3 +1,8 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AppStart {
+    public static void main(String[] args) {
+
+    }
 }
-dd
--- src/main/java/model/dto/ManagerDto.java
+++ src/main/java/model/dto/ManagerDto.java
@@ -1,4 +1,14 @@
 package model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ManagerDto {
+
 }
