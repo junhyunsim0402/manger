19ce3daabdd084f8d0d357009a249a9752fd5f03
junhyunsim0402 <simjjang11@gmail.com>
2026-02-27 15:14:11 +0900

    중간 푸쉬

--- src/main/java/model/dto/CategoryDto.java
+++ src/main/java/model/dto/CategoryDto.java
@@ -1,4 +1,13 @@
 package model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class CategoryDto {
 }
--- src/main/java/model/dto/VacationDto.java
+++ src/main/java/model/dto/VacationDto.java
@@ -1,4 +1,13 @@
 package model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class VacationDto {
 }
